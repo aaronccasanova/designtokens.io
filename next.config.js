aa7b48e09a15d2b5568704d3d583c6945e44acd5
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2021-11-14 23:50:56 -0800

    feat: Add styled-components support (#1)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    // TODO: Check back here for status updates on SWC/Styled Components support:
+    // https://github.com/vercel/next.js/discussions/30174#discussion-3643870
+    styledComponents: true,
+  },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`
+
+const theme = {}
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
-
-export default MyApp
