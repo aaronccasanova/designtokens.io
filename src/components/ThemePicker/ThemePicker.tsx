e31ada32342ed5c8d3c23edfddb8790efe3984a2
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2021-11-27 20:43:31 -0800

    fix: Add placeholder content to ThemePicker for better SSR support  (#5)

--- src/components/ThemePicker/ThemePicker.tsx
+++ src/components/ThemePicker/ThemePicker.tsx
@@ -1,60 +1,75 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { ThemeKey } from '../../designTokens'
 import { useRootTheme } from '../../providers'
+import { useSsr } from '../../hooks'
 
-const Root = styled('span')({
-  display: 'inline-block',
-  padding: '0 8px',
-  border: `1px solid var(--theme-color-text-primary)`,
-  backgroundColor: 'var(--theme-color-background-surface)',
-  ':focus-within': {
-    border: `1px solid var(--theme-color-primary-main)`,
-    outline: `1px solid var(--theme-color-primary-main)`,
-  },
-})
+const Root = styled.span`
+  display: inline-block;
+  padding: 0 8px;
+  border: 1px solid var(--theme-color-text-primary);
+  background-color: var(--theme-color-background-surface);
 
-const Select = styled('select')({
-  fontSize: 20,
-  padding: '8px 0px',
-  color: 'var(--theme-color-text-primary)',
-  textTransform: 'capitalize',
-  backgroundColor: 'transparent',
-  outline: 'none', // Focus styles handled in the root selector
-  border: 'none', // Border styles handled in the root selector
-})
+  &:focus-within {
+    border: 1px solid var(--theme-color-primary-main);
+    outline: 1px solid var(--theme-color-primary-main);
+  }
+`
 
-const Option = styled('option')({
-  fontSize: 20,
-  padding: '8px 0px',
-  color: 'var(--theme-color-text-primary)',
-  textTransform: 'capitalize',
-  backgroundColor: 'transparent',
-  outline: 'none', // Focus styles handled in the root selector
-  border: 'none', // Border styles handled in the root selector
-})
+const commonStyles = css`
+  font-size: 20px;
+  padding: 8px 0px;
+  color: var(--theme-color-text-primary);
+  text-transform: capitalize;
+  background-color: transparent;
+
+  /* Focus styles handled in the root selector */
+  outline: none;
+  /* Border styles handled in the root selector */
+  border: none;
+`
+
+const Select = styled.select`
+  ${commonStyles}
+`
+
+const Option = styled.option`
+  ${commonStyles}
+`
+
+const Placeholder = styled.span`
+  ${commonStyles}
+
+  display: inline-block;
+  padding: 8px;
+`
 
 export interface ThemePickerProps {
   className?: string
 }
 
 export const ThemePicker = (props: ThemePickerProps) => {
+  const { isServer } = useSsr()
   const { themeKey, themeKeys, setThemeKey } = useRootTheme()
 
   return (
     <Root {...props}>
-      <Select
-        value={themeKey}
-        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-          setThemeKey(e.target.value as ThemeKey)
-        }
-      >
-        {themeKeys?.map((theme) => (
-          <Option key={theme} value={theme}>
-            {theme}&nbsp;
-          </Option>
-        ))}
-      </Select>
+      {isServer ? (
+        <Placeholder>Theme</Placeholder>
+      ) : (
+        <Select
+          value={themeKey}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+            setThemeKey(e.target.value as ThemeKey)
+          }
+        >
+          {themeKeys?.map((theme) => (
+            <Option key={theme} value={theme}>
+              {theme}&nbsp;
+            </Option>
+          ))}
+        </Select>
+      )}
     </Root>
   )
 }
